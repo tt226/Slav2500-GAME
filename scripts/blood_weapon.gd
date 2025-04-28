439f12460446fdb3c0afd5909f9495c9f8da9f44
Brian Baugher <bdb7hq@virginia.edu>
2025-04-28 13:04:18 -0400

    fix up weapon collision

--- scripts/blood_weapon.gd
+++ scripts/blood_weapon.gd
@@ -4,18 +4,24 @@ extends Area2D
 @export var direction = Vector2.RIGHT
 @export var damage = 10
 
+@onready var timer = $Timer
+
 func _ready():
-    connect("area_entered", Callable(self, "_on_area_entered"))
-    $AnimatedSprite2D.play("default")  # Play the blood animation
-    get_node("Timer").start()  # Start the timer to auto-destroy
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	$AnimatedSprite2D.play("default")  # Play the blood animation
+	timer.start()  # Start the timer to auto-destroy
 
 func _physics_process(delta):
-    position += direction.normalized() * speed * delta
+	position += direction.normalized() * speed * delta
 
-func _on_Timer_timeout():
-    queue_free()
+func _on_body_entered(body):
+	print('body entered')
+	if body.is_in_group("enemies"):
+		print('enemy')
+		body.take_damage(damage)
+		queue_free()
 
-func _on_area_entered(area):
-    if area.is_in_group("enemies"):
-        area.take_damage(damage)
-        queue_free()
+
+func _on_timer_timeout() -> void:
+#	delete weapon after given time
+	queue_free()
