1ab18b72281349b264ada6b2d717c25707bda162
Brian Baugher <bdb7hq@virginia.edu>
2025-04-28 15:38:41 -0400

    vial adds health to player

--- scripts/blood_vial.gd
+++ scripts/blood_vial.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+const VIAL_STRENGTH = 15
 
-
-
-func _on_body_entered(_body: Node2D) -> void:
-	queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.increase_health(VIAL_STRENGTH)
+		queue_free()
