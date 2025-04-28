56ba77fd9a1349d16db812b6e01bfca6fa0cec44
Brian Baugher <bdb7hq@virginia.edu>
2025-04-28 13:29:12 -0400

    move health bar onto player

--- scripts/spear.gd
+++ scripts/spear.gd
@@ -15,6 +15,6 @@ func _on_Timer_timeout():
 func _on_body_entered(body: Node2D) -> void:
 	print('body entered from spear')
 	if body.name == "Player":
-		body.take_damage(1)
+		body.take_damage(10)
 		queue_free()
 	pass # Replace with function body.
